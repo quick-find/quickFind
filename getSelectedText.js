b2c2f5e54b346d9ef5f08439398c37ce6873ec62
Shiva Tamil Kumaran <anand.tk.03@gmail.com>
2022-02-05 16:30:05 +0000

    getSelectedText implemented

---
+++ getSelectedText.js
@@ -0,0 +1,9 @@
+function getSelectedText() {
+    if (window.getSelection) {
+        return window.getSelection().toString();
+    }
+    else if (document.selection) {
+        return document.selection.createRange().text;
+    }
+    return '';
+}
