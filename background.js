e8638f914667617dd141798ce8d1d08df52520a6
Samar <>
2022-02-05 16:51:15 +0000

    context menu

--- background.js
+++ background.js
@@ -4,3 +4,15 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+chrome.contextMenus.create({
+  id: "some-command",
+  title: "some title",
+  contexts: ["all"]
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId == "some-command") {
+      console.log("yay!");
+  }
+});
